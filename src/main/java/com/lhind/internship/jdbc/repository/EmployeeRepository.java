3ff3d9ce954e885e39752b17df2c2234fa2f6ab2
Elvis Barjami <e.barjami@teamsystem.com>
2025-02-19 11:31:59 +0100

    method update

--- src/main/java/com/lhind/internship/jdbc/repository/EmployeeRepository.java
+++ src/main/java/com/lhind/internship/jdbc/repository/EmployeeRepository.java
@@ -16,7 +16,10 @@ public class EmployeeRepository implements Repository<Employee, Integer> {
 
     private static final String SELECT_ALL = "SELECT * FROM employees;";
     private static final String SELECT_BY_ID = "SELECT * FROM employees WHERE employeeNumber = ?;";
-
+    private static final String CHECK_EXISTANCE = "SELECT 1 FROM employees WHERE employeeNumber = ?;";
+    private static final String UPDATE_EMPLOYEE = "UPDATE employees SET name = ?, email = ? WHERE employeeNumber = ?;";
+    private static final String INSERT_EMPLOYEE = "INSERT INTO employees (employeeNumber, name, email) VALUES (?, ?, ?);";
+    private static final String DELETE_EMPLOYEE = "DELETE FROM employees WHERE employeeNumber = ?;";
     private EmployeeMapper employeeMapper = EmployeeMapper.getInstance();
 
     @Override
@@ -55,6 +58,14 @@ public class EmployeeRepository implements Repository<Employee, Integer> {
     @Override
     public boolean exists(final Integer integer) {
         // TODO: Implement a method which checks if an employee with the given id exists in the employees table
+        try(final Connection connection = JdbcConnection.connect();
+            final PreparedStatement statement = connection.prepareStatement(CHECK_EXISTANCE)){
+            statement.setInt(1 , integer);
+            final ResultSet resultSet = statement.executeQuery();
+            return resultSet.next();
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
         return false;
     }
 
@@ -65,7 +76,41 @@ public class EmployeeRepository implements Repository<Employee, Integer> {
          *  If the employee exists then the method should instead update the employee
          *
          */
-        return null;
+
+        if(employee == null){
+            try {
+                throw new IllegalAccessException("Employee cannot be null");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try(final Connection connection = JdbcConnection.connect()){
+            if(exists(employee.getEmployeeNumber())){
+                try(final PreparedStatement statement = connection.prepareStatement(UPDATE_EMPLOYEE)){
+                    statement.setInt(1, employee.getEmployeeNumber());
+                    statement.setString(2, employee.getLastName());
+                    statement.setString(3, employee.getFirstName());
+                    statement.setString(4, employee.getExtension());
+                    statement.executeUpdate();
+                }
+            }else {
+                //create new  record
+                try (final PreparedStatement statement = connection.prepareStatement(INSERT_EMPLOYEE)) {
+                    statement.setInt(1, employee.getEmployeeNumber());
+                    statement.setString(2, employee.getLastName());
+                    statement.setString(3, employee.getFirstName());
+                    statement.setString(4, employee.getExtension());
+                    statement.setString(5, employee.getEmail());
+                    statement.setString(6, employee.getOfficeCode());
+                    statement.setInt(7, employee.getReportsTo());
+                    statement.setString(8, employee.getJobTitle());
+                    statement.executeUpdate();
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return employee;
     }
 
     @Override
@@ -73,5 +118,13 @@ public class EmployeeRepository implements Repository<Employee, Integer> {
         /*
          * TODO: Implement a method which deletes an employee given the id
          */
+        try(final Connection connection = JdbcConnection.connect();
+            final PreparedStatement statement = connection.prepareStatement(DELETE_EMPLOYEE)){
+            statement.setInt(1 , integer);
+            statement.executeUpdate();
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+
     }
 }
